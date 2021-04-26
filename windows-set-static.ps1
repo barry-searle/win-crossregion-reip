41ff46f7bd87ca7b28f484d46f1ad83c264439b5
Barry Searle <bsearle@google.com>
2021-04-26 13:50:58 -0500

    testing checkin

--- windows-set-static.ps1
+++ windows-set-static.ps1
@@ -1,4 +1,4 @@
-<# Copyright 2021 Barry Searle
+<#  Copyright 2021 Barry Searle
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
